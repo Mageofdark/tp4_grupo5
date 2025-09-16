dab9263c0e87cb39d2c5a4e4b12ea0c32ea4e960
FigueroaLucas <uuuuiikii@gmail.com>
2025-09-15 23:49:17 -0300

    Resolucion ejercicio 3

---
+++ src/ar/edu/unju/escmi/tp4/ejercicio3/Paciente.java
@@ -0,0 +1,65 @@
+package ar.edu.unju.escmi.tp4.ejercicio3;
+
+public class Paciente implements Comparable<Paciente> {
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private int numHC;
+    private String obraSocial;
+
+    public Paciente() {}
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public int getNumHC() {
+        return numHC;
+    }
+
+    public void setNumHC(int numHC) {
+        this.numHC = numHC;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    public void setObraSocial(String obraSocial) {
+        this.obraSocial = obraSocial;
+    }
+
+    public int compareTo(Paciente otroPaciente) {
+        return Integer.compare(this.numHC, otroPaciente.getNumHC());
+    }
+
+    public String toString(){
+        return "Paciente{" +
+                "dni=" + dni +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", numHC=" + numHC +
+                ", obraSocial='" + obraSocial + '\'' +
+                '}';
+    }
+}
